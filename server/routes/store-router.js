118613eb9603c17873f5b52289e0820284c59cde
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-04-07 19:01:56 -0400

    get items / by id

--- server/routes/store-router.js
+++ server/routes/store-router.js
@@ -10,13 +10,13 @@ router.get("/", async (req, res) => {
   }
 });
 
-// router.get("/", async (req, res) => {
-//     try {
-//       const items = await getAllItems();
-//       res.json(items);
-//     } catch (err) {
-//       res.json(err);
-//     }
-//   });
+router.get("/:id", async (req, res) => {
+    try {
+      const item = await getItemByID(req.params.id);
+      res.json(item);
+    } catch (err) {
+      res.json(err);
+    }
+  });
 
 module.exports = router;
