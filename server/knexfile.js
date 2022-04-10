86f834565a88262eacb0301084e4fc941ae3cd06
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-04-10 10:17:26 -0400

    successful connect

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,20 +1,24 @@
-// Update with your config settings.
+require('dotenv').config()
+const pg = require('pg')
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
- module.exports = {
+// if (process.env.DATABASE_URL) {
+//   pg.defaults.ssl = { rejectUnauthorized: false }
+// }
+
+const sharedConfig = {
+  client: 'pg',
+  migrations: { directory: './data/migrations' },
+  seeds: { directory: './data/seeds' },
+}
+
+module.exports = {
   development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/store.db3",
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
+    ...sharedConfig,
+    connection: process.env.DEV_DATABASE_URL,
   },
-};
+  production: {
+    ...sharedConfig,
+    connection: process.env.DATABASE_URL,
+    pool: { min: 2, max: 10 },
+  },
+}
