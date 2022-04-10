caf413d45f8e0986b5d150492cfd95934f620186
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-04-09 20:16:44 -0400

    cors?

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://anuenue.netlify.app");
   res.header("Access-Control-Allow-Credentials", true)
   res.header(
     "Access-Control-Allow-Headers",
