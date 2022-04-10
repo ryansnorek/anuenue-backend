a891d7f916018474d8f56cb65a9a5b6f2e860f3d
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-04-09 20:28:25 -0400

    cors??

--- server/server.js
+++ server/server.js
@@ -15,21 +15,21 @@ const storeRouter = require("./routes/store-router");
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors())
+// app.use(cors())
 app.use(
   cors({ origin: ["http://localhost:3000", "https://anuenue.netlify.app"]})
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://anuenue.netlify.app");
-  res.header("Access-Control-Allow-Credentials", true)
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "https://anuenue.netlify.app");
+//   res.header("Access-Control-Allow-Credentials", true)
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
+//   next();
+// });
 
 app.use("/stripe", stripeRouter);
 app.use("/store", storeRouter);
