26a3adc43c5441fc5faad174f2371fc60e715ee8
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-04-09 20:45:54 -0400

    cors

--- server/server.js
+++ server/server.js
@@ -5,36 +5,35 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.static("images"));
 
-// const cors = require("cors");
+const cors = require("cors");
 const helmet = require("helmet");
 const bodyParser = require("body-parser");
 
 const stripeRouter = require("./routes/stripe-router");
 const storeRouter = require("./routes/store-router");
 
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "https://anuenue.netlify.app");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
+//   if (req.method === "OPTIONS") {
+//     res.send(200);
+//   } else {
+//     next();
+//   }
+// });
 app.use(helmet());
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://anuenue.netlify.app");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
-  if (req.method === "OPTIONS") {
-    res.send(200);
-  } else {
-    next();
-  }
-});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cors())
 // app.use(
 //   cors({ origin: ["http://localhost:3000", "https://anuenue.netlify.app"]})
 // );
-
+app.use(cors({ origin: '*'}));
 
 
 app.use("/stripe", stripeRouter);
