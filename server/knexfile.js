79bbc151c2ca4dfbcd233273caf657d7e6e5896b
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-04-10 14:47:01 -0400

    prod

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const pg = require('pg')
 
-// if (process.env.DATABASE_URL) {
-//   pg.defaults.ssl = { rejectUnauthorized: false }
-// }
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = { rejectUnauthorized: false }
+}
 
 const sharedConfig = {
   client: 'pg',
