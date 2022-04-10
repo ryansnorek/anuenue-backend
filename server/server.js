d179bba097d72c2d5cfce377951cacf47ec11a2f
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-04-09 22:09:14 -0400

    w

--- server/server.js
+++ server/server.js
@@ -2,6 +2,13 @@ require("dotenv").config();
 const port = process.env.PORT || 8000;
 const express = require("express");
 const app = express();
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Max-Age: 1728000');
+header("Content-Length: 0");
+header("Content-Type: text/plain"); 
 
 // app.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", "https://anuenue.netlify.app");
@@ -21,21 +28,21 @@ app.use(express.static("public"));
 app.use(express.static("images"));
 
 const cors = require("cors");
-// const helmet = require("helmet");
+const helmet = require("helmet");
 const bodyParser = require("body-parser");
 
 const stripeRouter = require("./routes/stripe-router");
 const storeRouter = require("./routes/store-router");
 
-// app.use(helmet());
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(
-  cors({ origin: ["http://localhost:3000", "https://anuenue.netlify.app"] })
-);
+// app.use(
+//   cors({ origin: ["http://localhost:3000", "https://anuenue.netlify.app"] })
+// );
 
 app.use("/stripe", stripeRouter);
-// app.use("/store", storeRouter);
+app.use("/store", storeRouter);
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({
